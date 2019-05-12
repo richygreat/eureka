54604d21140fbd265509fefead86e51a6d90f44f
Richy Great <richygreat@gmail.com>
2019-05-12 12:16:21 +0200

    Aws specific changes

--- src/main/java/com/github/richygreat/eureka/EurekaApplication.java
+++ src/main/java/com/github/richygreat/eureka/EurekaApplication.java
@@ -2,12 +2,29 @@ package com.github.richygreat.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
+import com.netflix.appinfo.AmazonInfo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		b.setDataCenterInfo(info);
+		log.info("AmazonInfo: {}", info);
+		return b;
 	}
 }
